d46c90755b77e4418dcadca225d4732eb0a43e66
Afsal Thaj <afsal.taj06@gmail.com>
2021-04-14 10:56:09 +1000

    Remove example

--- core/src/main/scala/com/thaj/scala/three/generic/smarconstructors/InlineTransparentExample.scala
+++ core/src/main/scala/com/thaj/scala/three/generic/smarconstructors/InlineTransparentExample.scala
@@ -8,9 +8,3 @@ object Natural:
    transparent inline def apply(v: Int): Option[Natural]  = 
      if (v > 0) Some(new Natural(v)) else None
 end Natural
-
-object InlineTransparentExample extends App:
-  val valid: Some[Natural] = Natural(1) 
-  val invali: None.type = Natural(-1)
-  println(valid.get)    
-end InlineTransparentExample
